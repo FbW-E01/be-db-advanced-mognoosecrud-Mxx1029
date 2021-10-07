890bbd586c90a4a89282143745e8e6e9fe06c15d
Mxx1029 <lia.pim@icloud.com>
2021-10-07 11:14:33 +0200

    Put event handlers and connection string into  connect() function in connection.js

--- connection.js
+++ connection.js
@@ -1,26 +1,22 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // $ mongo
 // > use mongooseCrudExercise
 // > db.createUser({ user: "jocelyn", pwd: "passw0rd", roles: ["readWrite"]} )
 
-
-
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT } = process.env;
-const db = 'mongooseCrudExercise';
-
-const connectionString = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${db}`;
-
-mongoose.connection.on('error', (err) => console.log(">> Error ", err) || process.exit(0));
-mongoose.connection.on('connecting', () => console.log(">> Connecting"))
-mongoose.connection.on('connected', () => console.log(">> Connected"))
-mongoose.connection.on('disconnecting', () => console.log(">> Disconnecting"))
-mongoose.connection.on('disconnected', () => console.log(">> Disconnected"))
-
 export async function connect() {
+    // as you export it you don't need the dotenv here
+    const { DB_USER, DB_PASS, DB_HOST, DB_PORT } = process.env;
+    const db = 'mongooseCrudExercise';
+    
+    const connectionString = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${db}`;
+    
+    mongoose.connection.on('error', (err) => console.log(">> Error ", err) || process.exit(0));
+    mongoose.connection.on('connecting', () => console.log(">> Connecting"))
+    mongoose.connection.on('connected', () => console.log(">> Connected"))
+    mongoose.connection.on('disconnecting', () => console.log(">> Disconnecting"))
+    mongoose.connection.on('disconnected', () => console.log(">> Disconnected"))
+    
     await mongoose.connect(connectionString);
 }
 
